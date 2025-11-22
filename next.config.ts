e0465d0cd253305dc3b6a8ff5ccecf3e9be33371
gabrielalmir <gabr.almir@gmail.com>
2025-11-21 22:15:03 -0300

    feat: add experimental optimizations in next.config.ts for package imports and CSS

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   compress: true,
+  experimental: {
+    optimizePackageImports: ['lucide-react', 'devicons-react', 'framer-motion'],
+    optimizeCss: true,
+  }
 };
 
 export default nextConfig;
